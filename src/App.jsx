da99b57e802a1670828c61aceb4c5fbc6bf513df
elisha <korosenseimark40@gmail.com>
2025-09-16 00:02:12 +0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import React, { use, useEffect, useState } from 'react';
+// import logo from './logo.svg';
+import Search from './components/Search';
+import './App.css';
+
+const App = () => {
+const[searchTerm, setSearchTerm] = useState('')
+
+
+  return (
+    <main>
+     <div className='pattern'> 
+     <div className='wrapper'> 
+      <header>  <
+        img src="./hero-img.png" alt="Banner" />
+        <h1>find  <span className='text-gradient'>Movies</span> you'll Enojoy without the Hassle</h1>
+      </header>
+
+      <header>
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      </header>
+
+     </div>
+    </div>
+    </main>
+  ) 
+  
+}
+
+
+export default App;
